502460a059cad9fa653a6fd371a4af624833fde4
xiaobin <your_emial@examle.com>
2025-11-28 19:34:51 +0800

    删除测试模块下的unit文件，测试文件都在.cpp进行测试。 修复core模块下找不到QtEndian等头文件的问题

--- src/core/message.h
+++ src/core/message.h
@@ -1,4 +1,23 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
+#include<QtEndian>
+#include<QtDebug>
+
+namespace CoreModule {
+
+//消息枚举类型
+enum class MsgType:int{
+    REGISTER=1,
+    LOGIN=2,
+    Quit=3,
+    SEARCH=4,
+    HISTORY=5,
+    HEARTBEAT=6,
+    COLLECT=7,
+    QUERYCOLLECT=8
+};
+
+}
+
 
 #endif // MESSAGE_H
--- src/tests/tst_coretest.cpp
+++ src/tests/tst_coretest.cpp
@@ -1,6 +1,6 @@
 ﻿#include <gtest/gtest.h>
 #include "core/core.h"
-
+#include"core/message.h"
 using namespace CoreModule;
 
 // 基础函数测试
@@ -20,7 +20,19 @@ TEST(CoreTests, CoreHandlerBasicTest) {
     EXPECT_TRUE(handler.isReady());
 }
 
-// 基础断言
-TEST(CoreTests, BasicTest) {
-    EXPECT_EQ(1, 1);
+
+TEST(CoreTests,CoreMessageTest)
+{
+    //消息类型测试，验证每个枚举成员的数值是否符合定义
+    EXPECT_EQ(static_cast<int>(MsgType::REGISTER),1);
+    EXPECT_EQ(static_cast<int>(MsgType::LOGIN),2);
+    EXPECT_EQ(static_cast<int>(MsgType::Quit),3);
+    EXPECT_EQ(static_cast<int>(MsgType::SEARCH),4);
+    EXPECT_EQ(static_cast<int>(MsgType::HISTORY),5);
+    EXPECT_EQ(static_cast<int>(MsgType::HEARTBEAT),6);
+    EXPECT_EQ(static_cast<int>(MsgType::COLLECT),7);
+    EXPECT_EQ(static_cast<int>(MsgType::QUERYCOLLECT),8);
+    //验证MsgType类型是否为int
+    EXPECT_EQ(sizeof(MsgType),sizeof(int));
+
 }
